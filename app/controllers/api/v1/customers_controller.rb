b203c149126c7187f653179b0fad79074dc15310
SofiaFredriksson <sofiapkf@gmail.com>
2019-08-05 11:51:07 +0100

    Customer create controller method

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::CustomersController < ApplicationController
         @customers = Customer.all 
         render json: @customers
     end
+
+    def create 
+        @customer = Customer.new(customer_params)
+
+        if @customer.save
+            render json: @customer
+        else
+            render json: { errors: "Customer could not be created, please try again." }, status: 404
+        end 
+    end 
+
+    private 
+
+    def customer_params
+        params.require(:customer).permit(:name)
+    end 
 end
